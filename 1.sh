bf771958097e0fd195798498f89ec8fe1bb3ae06
koolwithk <alok.mauryaa@gmail.com>
2022-02-05 18:05:02 +0530

    1.1.4 update

--- 1.sh
+++ 1.sh
@@ -17,6 +17,22 @@ run(){
     else
         echo "FAIL,$module_1_1_2" | tee -a $output
     fi
+
+    module_1_1_3="1.1.3 Ensure that the controller manager pod specification file permissions are set to 644 or more restrictive (Automated)"
+    if [ "$(stat -c %a /etc/kubernetes/manifests/kube-controller-manager.yaml)" -lt "644" ]
+    then
+        echo "PASS,$module_1_1_3" | tee -a $output
+    else
+        echo "FAIL,$module_1_1_3" | tee -a $output
+    fi
+
+    module_1_1_4="1.1.4 Ensure that the controller manager pod specification file ownership is set to root:root (Automated)"
+    if [ "$(stat -c %U:%G /etc/kubernetes/manifests/kube-controller-manager.yaml)" == "root:root" ]
+    then
+        echo "PASS,$module_1_1_4" | tee -a $output
+    else
+        echo "FAIL,$module_1_1_4" | tee -a $output
+    fi
 }
 
 run
